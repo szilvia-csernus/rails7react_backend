c240a3b3d89eec8dc8001fe263fc175f03836300
szilvi <csernus.szilvi@gmail.com>
2022-08-14 17:34:36 +0100

    get users route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :notes
-  resource :users, only: [:create]
+  resource :users
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
 end
