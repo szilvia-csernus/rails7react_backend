289e7ab48838587d9b3acd66e7c968f75efa9fcf
szilvi <csernus.szilvi@gmail.com>
2022-09-11 17:36:15 +0100

    change in login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
     #LOGGING IN
     def login
-        # @user = User.find_by(username: params[:username])
+        @user = User.find_by(username: params[:username])
 
         if @user && @user.authenticate(params[:password])
             token = encode_token({user_id: @user.id})
